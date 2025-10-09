f1a2706757d133dcf8451a88d52eb8bf728aa597
Oleg Zharov <levineye@gmail.com>
2025-10-10 00:03:39 +0300

    feat: add mux, server

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func getStatus() {}
+func hello(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("hello")
+}
 
 func main() {
-  
+	router := http.NewServeMux()
+
+	router.HandleFunc("/hello", hello)
+
+	server := http.Server{
+		Addr:    "localhost:8082",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
